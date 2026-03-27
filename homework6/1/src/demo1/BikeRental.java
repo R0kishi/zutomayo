aca195396835ac80b678ef73143fdbc9cf906c1a
R0k1shi <2120407825@qq.com>
2026-03-27 14:33:00 +0800

    Add files via upload

---
+++ homework6/1/src/demo1/BikeRental.java
@@ -0,0 +1,109 @@
+package demo1;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.Scanner;
+public class BikeRental {
+    private boolean isRegisteredUser;
+    private String emailAddress;
+    private String location;
+    private LocalDateTime tripStartTime;
+    private String bikeID;
+    private boolean locationValid;
+    private UserRegistration userRegistration = new UserRegistration();
+    private ActiveRental activeRental;
+    private LinkedList<ActiveRental> activeRentalsList = new LinkedList<>();
+    public void simulateApplicationInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("This is the simulation of the e-bike rental process.");
+        // 输入信息
+        System.out.print("Are you a registered user? (true/false): ");
+        isRegisteredUser = sc.nextBoolean();
+        System.out.print("Enter your email: ");
+        emailAddress = sc.next();
+        System.out.print("Enter rental location: ");
+        location = sc.next();
+        System.out.println("Simulating the analysis of the rental request.");
+        bikeID = analyseRequest(isRegisteredUser, emailAddress, location);
+        if (!locationValid) {
+            return;
+        }
+        System.out.println("Simulating e-bike reservation…");
+        reserveBike(bikeID);
+        System.out.println("Displaying the active rentals…");
+        viewActiveRentals();
+        System.out.println("Simulating the end of the trip…");
+        removeTrip(bikeID);
+        System.out.println("Displaying the active rentals after trip end…");
+        viewActiveRentals();
+    }
+    private String analyseRequest(boolean isRegisteredUser, String emailAddress, String location) {
+        if (isRegisteredUser) {
+            System.out.println("Welcome back, " + emailAddress + "!");
+        } else {
+            System.out.println("You’re not our registered user. Please consider registering.");
+            userRegistration.registration();
+        }
+        return validateLocation(location);
+    }
+    private String validateLocation(String location) {
+        for (Bike bike : BikeDataBase.bikes) {
+            if (bike.getLocation().equals(location) && bike.isAvailable()) {
+                System.out.println("A bike is available at the location you requested.");
+                locationValid = true;
+                return bike.getBikeID();
+            }
+        }
+        System.out.println("Sorry, no bikes are available at the location you requested. Please try again later.");
+        locationValid = false;
+        return null;
+    }
+    private void reserveBike(String bikeID) {
+        if (bikeID == null) {
+            System.out.println("Sorry, we’re unable to reserve a bike at this time. Please try again later.");
+            return;
+        }
+        for (Bike bike : BikeDataBase.bikes) {
+            if (bike.getBikeID().equals(bikeID)) {
+                tripStartTime = LocalDateTime.now();
+                // 改状态
+                bike.setAvailable(false);
+                bike.setLastUsedTime(tripStartTime);
+                System.out.println("Reserving the bike with the " + bikeID + ". Please following the on-screen instructions to locate the bike and start your pleasant journey.");
+                // 初始化并加入链表
+                activeRental = new ActiveRental(bikeID, emailAddress, tripStartTime);
+                activeRentalsList.add(activeRental);
+                break;
+            }
+        }
+    }
+    private void viewActiveRentals() {
+        if (activeRentalsList.isEmpty()) {
+            System.out.println("No active rentals at the moment.");
+            return;
+        }
+        for (ActiveRental ar : activeRentalsList) {
+            System.out.println(ar);
+        }
+    }
+    private void removeTrip(String bikeID) {
+        ActiveRental targetRental = null;
+        for (ActiveRental ar : activeRentalsList) {
+            if (ar.getBikeID().equals(bikeID)) {
+                targetRental = ar;
+                break;
+            }
+        }
+        if (targetRental != null) {
+            activeRentalsList.remove(targetRental);
+        }
+        for (Bike bike : BikeDataBase.bikes) {
+            if (bike.getBikeID().equals(bikeID)) {
+                bike.setAvailable(true);
+                bike.setLastUsedTime(LocalDateTime.now());
+                System.out.println("Your trip has ended. Thank you for riding with us.");
+                break;
+            }
+        }
+    }
+
+}
---
+++ homework6/1/src/demo1/test.java
@@ -0,0 +1,25 @@
+package demo1;
+
+public class test {
+    public static void main(String[] args) {
+                ERyder bike1 = new ERyder();
+                bike1.printBikeDetails();
+                bike1.printUsageDetails(24);
+                ERyder bike2 = new ERyder("R0k1shi", 75, true, 120.5,"Mikasa",1995888);
+                bike2.printUsageDetails(64);
+                bike2.ride();
+                bike2.printBikeDetails();
+                String sent1 = "I was very satisfied with the service.";
+                String sent2 = "The e-Bike is quite comfortable to ride.";
+                String sent3 = "The battery life of the e-Bike is impressive.";
+                String sent4 = "The customer support was helpful and responsive.";
+                String sent5 = "I would recommend this e-Bike to my friends and family.";
+                Feedback userFeedback = new Feedback("Liu", "liyang", "Liuliyang@R0k1shi.com");
+                userFeedback.analyseFeedback(true, sent1, sent2, sent3, sent4, sent5);
+                System.out.println(userFeedback);
+                UserRegistration registration = new UserRegistration();
+                registration.registration();
+                AdminPanel admin = new AdminPanel();
+                admin.userManagementOptions();
+    }
+}
